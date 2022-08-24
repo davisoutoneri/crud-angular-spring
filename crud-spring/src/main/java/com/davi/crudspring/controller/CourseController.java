bddb1cb75c0a6fecc7b50e249b6600636e30d39e
davylander <101679754+davylander@users.noreply.github.com>
2022-08-24 18:52:29 -0300

    Adicionando conclusão das aulas até a 21

--- crud-spring/src/main/java/com/davi/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/davi/crudspring/controller/CourseController.java
@@ -1,8 +1,14 @@
 package com.davi.crudspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.davi.crudspring.model.Course;
@@ -27,8 +33,20 @@ public class CourseController {
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public List<Course> list(){
-    return courseRepository.findAll();
-  }
+    public List<Course> list() {
+        return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    //http status 201 = ResponseEntity / boas práticas
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED) // mais eficiente e simplificado,clean code
+    public /*ResponseEntity<Course>*/Course create(@RequestBody Course course) {
+        //System.out.println(course.getName());
+        return courseRepository.save(course);
+        //return ResponseEntity.status(HttpStatus.CREATED).body(course);
+    }
+
+    //http status 201
 
 }
